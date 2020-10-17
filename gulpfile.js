8c3944da41b75afc98fb5b0a7da56595ac117d68
yamamoto syo <syo19961113@gmail.com>
2020-10-17 17:45:46 +0900

    add package.json package-lock.json gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+const { src, dest, watch } = require('gulp');
+
+const task = () => {
+    return src('src/sass/**/*.scss', {sourcemaps: true})
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(dest('dist/css/', {sourcemaps: true}))
+}
+
+const keiryoWatch = () => {
+    watch('sass/*.scss', task)
+}
+exports.keiryoWatch = keiryoWatch;
