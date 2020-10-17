da02893054f1a5ff9f5b11fda1e74e1c1f0b0669
yamamoto syo <syo19961113@gmail.com>
2020-10-17 17:05:29 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tekeloca | 出張写真撮影サービス</title>
+</head>
+<body>
+    
+</body>
+</html>
