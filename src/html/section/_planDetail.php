835efb82683d96185265cec135e173b1686e9593
yamamoto syo <syo19961113@gmail.com>
2020-10-21 08:57:30 +0900

    finish plan01 ~ plan03

--- src/html/section/_planDetail.php
+++ src/html/section/_planDetail.php
@@ -1,15 +1,15 @@
 <section class="planDetail">
-    <div class="planDetail-container">
+    <div id="plan01" class="planDetail-container plan01">
         <div class="planDetail-num-container">
-            <span class="line"></span>
-            <p class="planDetail-num">plan<span>01</span></p>
-            <span class="line"></span>
+            <span class="line__blue"></span>
+            <p class="planDetail-num">plan<span class="blue">01</span></p>
+            <span class="line__blue"></span>
         </div>
         
         
         <div class="planDetail-title">
             <h3 class="planDetail-title__sub">もっと自分を好きになれる！</h3>
-            <h2 class="planDetail-title__main"><span>アイコン撮影</span>プラン</h2>
+            <h2 class="planDetail-title__main"><span class="blue">アイコン撮影</span>プラン</h2>
         </div>
 
         <p class="planDetail-text">今やSNSなどで必須になっているアイコン。もっと自分の良さが出た写真が欲しいと思っている方向けのプランです。<br>
@@ -35,13 +35,104 @@
         <p class="scroll">横にスクロールしてください</p>
 
         <div class="btn-container">
-            <button class="btn btn-bgc_blue">
+            <button class="btn btn-bgc__blue">
                 <a href="#" class="arrow-u">撮影料金を見る</a>
             </button>
 
-            <button class="btn btn-bgc_yellow">
+            <button class="btn btn-bgc__yellow">
                 <a href="#" class="arrow-u">申込フォームへ</a>
             </button>
         </div>
     </div>
+
+    <div id="plan02" class="planDetail-container plan02">
+        <div class="planDetail-num-container">
+                <span class="line__pink"></span>
+                <p class="planDetail-num">plan<span class="pink">02</span></p>
+                <span class="line__pink"></span>
+            </div>
+            
+            
+            <div class="planDetail-title">
+                <h3 class="planDetail-title__sub">お二人の素敵な写真を残します！</h3>
+                <h2 class="planDetail-title__main"><span class="pink">カップル撮影</span>プラン</h2>
+            </div>
+
+            <p class="planDetail-text">二人での綺麗な写真が欲しい、二人の写真がもっと欲しい。そんな声にお応えできるプラン。<br>
+            自然な笑顔や、何気ない仕草を逃さずシャッターを切ります！<br>
+            サプライズなどもお手伝いいたしますので、お気軽に提案してください。
+            </p>
+
+            <div class="planDetail-photo">
+                <img src="../images/couple_01.jpg" alt="" class="imgBig">
+
+                <ul class="planDetail-photo-small">
+                    <li><img src="../images/couple_01.jpg" alt="" class="imgSmall"></li>
+                    <li><img src="../images/couple_02.jpg" alt="" class="imgSmall"></li>
+                    <li><img src="../images/couple_03.jpg" alt="" class="imgSmall"></li>
+                    <li><img src="../images/couple_04.jpg" alt="" class="imgSmall"></li>
+                </ul>
+            
+            </div>
+
+            <div class="planDetail-step">
+                <img src="../images/step.jpg" class="imgStep" alt="撮影前に打ち合わせをしてから、2箇所の撮影スポットで撮影します。データは1週間以内に送らせて頂きます。">
+            </div>
+            <p class="scroll">横にスクロールしてください</p>
+
+            <div class="btn-container">
+                <button class="btn btn-bgc__pink">
+                    <a href="#" class="arrow-u">撮影料金を見る</a>
+                </button>
+
+                <button class="btn btn-bgc__yellow">
+                    <a href="#" class="arrow-u">申込フォームへ</a>
+                </button>
+            </div>
+        </div>
+    </div>
+    
+    <div id="plan03" class="planDetail-container plan03">
+        <div class="planDetail-num-container plan03">
+                <span class="line__green"></span>
+                <p class="planDetail-num">plan<span class="green">03</span></p>
+                <span class="line__green"></span>
+            </div>
+            
+            
+            <div class="planDetail-title">
+                <h3 class="planDetail-title__sub">一生の思い出を鮮やかに。</h3>
+                <h2 class="planDetail-title__main"><span class="green">結婚式撮影</span>プラン</h2>
+            </div>
+
+            <p class="planDetail-text">一生の思い出に残る結婚式。そんな日だからこそ、写真に残したい。<br>
+            写真を見るだけで、大切な日の思い出が蘇ってくる写真を撮ります。<br>
+            お二人のイメージがあればなんなりとお申し付けください。
+            </p>
+
+            <div class="planDetail-photo">
+                <img src="../images/wedding_01.jpg" alt="" class="imgBig">
+
+                <ul class="planDetail-photo-small">
+                    <li><img src="../images/wedding_01.jpg" alt="" class="imgSmall"></li>
+                    <li><img src="../images/wedding_02.jpg" alt="" class="imgSmall"></li>
+                    <li><img src="../images/wedding_03.jpg" alt="" class="imgSmall"></li>
+                    <li><img src="../images/wedding_04.jpg" alt="" class="imgSmall"></li>
+                </ul>
+            
+            </div>
+
+            <div class="btn-container">
+                <button class="btn btn-bgc__green">
+                    <a href="#" class="arrow-u">撮影料金を見る</a>
+                </button>
+
+                <button class="btn btn-bgc__yellow">
+                    <a href="#" class="arrow-u">申込フォームへ</a>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    
 </section>
